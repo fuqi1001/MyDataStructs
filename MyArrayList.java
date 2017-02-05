c65ee476d49dc58661fb54a96b9a2e971fafa7ea
qifu <fuqiqi12345@gmail.com>
2017-02-05 08:16:05 -0500

    MyArrayList

---
+++ MyArrayList.java
@@ -0,0 +1,10 @@
+/**
+ * Created by qifu on 17/2/5.
+ */
+public class MyArrayList<AnyType> implements Iterable<AnyType> {
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private int theSize;
+    private AnyType [] theItems;
+
+}
